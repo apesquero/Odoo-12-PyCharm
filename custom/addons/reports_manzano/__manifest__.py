79b536078b7f82aa05a231ca06cfd101715335ca
apesquero <apesquero@gmail.com>
2018-12-03 00:04:50 +0100

    Update files

---
+++ custom/addons/reports_manzano/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Reports Manzanodecora',
+    'description': 'Módulo Personalización Informes Manzanodecora',
+    'version': '12.0.1.0.0',
+    'author': 'Amaro Pesquero',
+    'depends': ['web', 'account', 'sale', 'payment_signal', 'custom_partner', ],
+    'data': [
+        'views/report_templates.xml',
+        'views/report_layout.xml',
+        'views/report_saleorder.xml',
+        'data/paper_formats.xml',
+    ],
+    'category': 'Sales',
+    'installable': True,
+    'application': False,
+}
