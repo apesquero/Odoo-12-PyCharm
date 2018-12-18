5a85f98f20ef3c3a5245965d804ae2173105cd25
apesquero <apesquero@gmail.com>
2018-12-18 22:31:12 +0100

    Update reports_manzano

--- custom/addons/reports_manzano/__manifest__.py
+++ custom/addons/reports_manzano/__manifest__.py
@@ -11,6 +11,7 @@
         'views/report_layout.xml',
         'views/report_saleorder.xml',
         'report/sale_report_templates.xml',
+        'report/invoice_report_templates.xml',
     ],
     'category': 'Sales',
     'installable': True,
