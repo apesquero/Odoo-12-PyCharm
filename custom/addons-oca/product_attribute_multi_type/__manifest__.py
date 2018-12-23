b5c4aad2dd286368ec8a21710e15ecb20aa166f2
apesquero <apesquero@gmail.com>
2018-12-24 00:36:44 +0100

    Update reports_manzano

---
+++ custom/addons-oca/product_attribute_multi_type/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Product attribute types',
+    'description': 'This module extends product attributes, adds 2 new types: range & numeric',
+    'version': '12.0.1.0.0',
+    'author': 'Amaro Pesquero',
+    'depends': ['product', ],
+    'data': [
+        # 'views/product_view.xml',
+        # 'data/product_attribute_data.xml',
+    ],
+    'category': 'Product Management',
+    'installable': True,
+    'application': False,
+}
