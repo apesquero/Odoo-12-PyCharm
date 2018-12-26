83808f87cd691b9f1a0f16a6782591ff6d03bdd0
apesquero <apesquero@gmail.com>
2018-12-27 00:52:49 +0100

    Update product_attribute_type

--- custom/addons/product_attribute_type/models/product_attribute.py
+++ custom/addons/product_attribute_type/models/product_attribute.py
@@ -12,8 +12,7 @@ class ProductAttributeValue(models.Model):
     _inherit = 'product.attribute.value'
 
     image = fields.Binary(string='Image')
-    attr_type = fields.Selection(string='Type',
-                                 related='attribute_id.type')
+
     numeric_value = fields.Float('Numeric Value',
                                  digits=dp.get_precision('Product Attribute'))
     min_range = fields.Float('Min',
@@ -21,21 +20,18 @@ class ProductAttributeValue(models.Model):
     max_range = fields.Float('Max',
                              digits=dp.get_precision('Product Attribute'))
 
-    @api.constrains('min_range', 'max_range')
-    def _check_min_max_range(self):
-        for value in self:
-            # we check only values of range type attributes.
-            if value.type != 'range':
-                continue
-            if value.min_range > value.max_range:
-                raise exceptions.Warning(
-                    _('The min range should be less than the max range.'))
-        return True
 
 class ProductTemplateAttributeValue(models.Model):
     _inherit = "product.template.attribute.value"
 
     image = fields.Binary(string='Image', related="product_attribute_value_id.image")
     numeric_value = fields.Float('Numeric Value',
-                                 digits=dp.get_precision('Product Attribute'))
+                                 digits=dp.get_precision('Product Attribute'),
+                                 related="product_attribute_value_id.numeric_value")
+    min_range = fields.Float('Min',
+                             digits=dp.get_precision('Product Attribute'),
+                             related="product_attribute_value_id.min_range")
+    max_range = fields.Float('Max',
+                             digits=dp.get_precision('Product Attribute'),
+                             related="product_attribute_value_id.max_range")
 
