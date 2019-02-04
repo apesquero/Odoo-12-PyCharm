b05c48ef215e381b122c80969572ddda1a8ebf10
apesquero <apesquero@gmail.com>
2019-02-04 18:43:38 +0100

    Create module price_extra_percent

--- custom/addons/price_extra_percent/models/product_attribute.py
+++ custom/addons/price_extra_percent/models/product_attribute.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ProductTemplateAttributeValue(models.Model):
@@ -14,3 +14,12 @@ class ProductTemplateAttributeValue(models.Model):
 
     percentage = fields.Integer(string='Percent Extra Price',
                                 default=0)
+
+    @api.onchange('price_extra_type')
+    def _compute_price_extra(self):
+        for product in self:
+            if product.price_extra_type == 'percentage':
+                # list_price = self.env['product.template'].search([]).list_price
+                list_price = 100
+                price_percent = (product.percentage * list_price) / 100
+                product.update({'price_extra': product.price_extra + price_percent})
