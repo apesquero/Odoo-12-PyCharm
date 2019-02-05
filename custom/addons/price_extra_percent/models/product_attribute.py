ce6a8c9e06a3ac0c017e89af0d41d633e9da36a2
apesquero <apesquero@gmail.com>
2019-02-05 19:36:51 +0100

    Create module price_extra_percent

--- custom/addons/price_extra_percent/models/product_attribute.py
+++ custom/addons/price_extra_percent/models/product_attribute.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api
+from odoo.addons import decimal_precision as dp
 
 
 class ProductTemplateAttributeValue(models.Model):
@@ -15,11 +16,25 @@ class ProductTemplateAttributeValue(models.Model):
     percentage = fields.Integer(string='Percent Extra Price',
                                 default=0)
 
+    price_extra2 = fields.Float(
+        string='Attribute Price Extra',
+        default=0.0,
+        compute='_compute_price_extra',
+        compute_sudo=False,
+        digits=dp.get_precision('Product Price'),
+        help="""Price Extra: Extra price for the variant with
+            this attribute value on sale price. eg. 200 price extra, 1000 + 200 = 1200.""")
+
+    @api.depends('percentage')
     @api.onchange('price_extra_type')
     def _compute_price_extra(self):
         for product in self:
             if product.price_extra_type == 'percentage':
-                # list_price = self.env['product.template'].search([]).list_price
-                list_price = 100
+                active_id = product.product_tmpl_id._context['active_id']
+                list_price = self.env['product.template'].search(
+                    [('id', '=', active_id)]).list_price
                 price_percent = (product.percentage * list_price) / 100
-                product.update({'price_extra': product.price_extra + price_percent})
+                product.update(
+                    {'price_extra': price_percent,
+                     'price_extra2': price_percent
+                     })
